f1a5228f1940f1138a94085e5e6f63d28ef46d0a
Doa <156461653+doaaltay@users.noreply.github.com>
2024-04-02 17:47:05 +0000

    nutrition

--- recipes/api_nutrition.js
+++ recipes/api_nutrition.js
@@ -6,7 +6,36 @@ import { ingredients } from './pages/menemen.js';
 import { ingredients } from './pages/cheesecake.js';
 import { ingredients } from './pages/chickensalad.js';
 import { ingredients } from './pages/sphagetti.js';
+/**
+ 
+//add sth to make sure 1000 requirest max- how to test this?
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
+function App() {
+  const [nutritiondata, setNutritionaData] = useState([]);
+
+  useEffect(() => {
+    axios.get('add the link here')
+      .then(response => {
+        setPosts(response.data);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }, []);
+
+  return (
+    <ul>
+      {posts.map(post => (
+        <li key={post.id}>{post.title}</li>
+      ))}
+    </ul>
+  );
+}
+
+export default App;
+ */
 
 
 
